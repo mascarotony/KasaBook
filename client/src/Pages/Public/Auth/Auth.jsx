7c509737243970349f4e3643f8de14e77709e02b
Mascaro Tony <mascaro.tony@outlook.fr>
2023-02-06 23:20:28 +0100

    #7 Front -> ajout fonction changeCard
    -> Ajout de la fonction changeCard sur la page Auth pour swiper entre le composant Login et le composant Signup.
    
    -> Par défaut, le composant Login sera afficher ( pour éviter à un utilisateur déjà inscrit de repasser par le formulaire d'incription )
    
    -> Retouche de style pour lapage Auth pour centrer les composants

--- client/src/Pages/Public/Auth/Auth.jsx
+++ client/src/Pages/Public/Auth/Auth.jsx
@@ -1,5 +1,6 @@
 //Utils
-import React from 'react'
+import React, { useState } from 'react'
+import { Helmet } from 'react-helmet'
 
 //Components
 import { SignUp, Login } from '../../../Components/Contributor/index'
@@ -13,24 +14,38 @@ import './Auth.css'
 
 //Function
 function Auth() {
-  return ( 
+  const [display, setDisplay] = useState('none')
+  const [isActive, setIsActive] = useState('')
+
+  function showCard() {
+    setIsActive(isActive === '' ? 'active' : '')
+    setDisplay(display === 'block' ? 'none' : 'block')
+  }
+
+  return (
     <div className="AuthPage">
+      <Helmet>
+        <title>KasaBook | Mon Compte</title>
+      </Helmet>
       <div className="LoginBloc">
         <b className="LogTitle">Connexion</b>
         <Login />
         <p>
-          Pas encore inscris ?{' '}
-          <button>
+          Pas encore inscrit ?{' '}
+          <button onClick={showCard}>
             Lancez-vous !<FontAwesomeIcon icon={faChevronRight} />
           </button>
         </p>
       </div>
-      <div className="SignUpBloc">
+      <div
+        className={`SignUpBloc ${isActive}`}
+        style={{ display: `${display}` }}
+      >
         <b className="SUTitle">Inscription</b>
         <SignUp />
         <p>
           Vous êtes déjà inscrit ?{' '}
-          <button>
+          <button onClick={showCard}>
             Connectez-vous ! <FontAwesomeIcon icon={faChevronRight} />
           </button>
         </p>
