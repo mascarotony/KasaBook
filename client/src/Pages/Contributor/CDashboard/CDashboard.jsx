b5ec895567be7016b43a556c9b275def92b26dc4
Mascaro Tony <mascaro.tony@outlook.fr>
2023-02-10 17:16:06 +0100

    #11 Front -> Création du DashBoard
    -> Création du contenu de la page dashboard
    
    -> Début de mise en forme de cette dernière

--- client/src/Pages/Contributor/CDashboard/CDashboard.jsx
+++ client/src/Pages/Contributor/CDashboard/CDashboard.jsx
@@ -8,7 +8,22 @@ import './CDashboard.css'
 function CDashboard() {
   return (
     <div className="CDashboard">
-      <b className="Title">Tableau de bord</b>
+      <b className="Title">Bonjour *Pseudo*</b>
+      <div className="UserDash">
+        <div className="UserPic">
+          <div className="UserImg"></div>
+          <button>Changer de photo de profil</button>
+        </div>
+        <div className="UserInfo">
+          <div className="UserDesc">
+            <p>*Pseudo*</p>
+            <p>*email*</p>
+            <p>*password*</p>
+            <p>*description*</p>
+          </div>
+          <button>Sauvegarder mes changements</button>
+        </div>
+      </div>
     </div>
   )
 }
